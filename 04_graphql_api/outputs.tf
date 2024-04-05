d2cdaffe2c88c7fe155b9d95a01a1931e286865d
Michael Munsch <Mi3.1415926@gmail.com>
2024-04-05 20:10:31 +0200

    chore: add aws_appsync_graphql_api arn to output

--- 04_graphql_api/outputs.tf
+++ 04_graphql_api/outputs.tf
@@ -5,3 +5,7 @@ output "jobs_danymodb_table_arn" {
 output "jobs_danymodb_table_name" {
   value = aws_dynamodb_table.jobs.name
 }
+
+output "aws_appsync_graphql_api_arn" {
+  value = aws_appsync_graphql_api.this.arn
+}
