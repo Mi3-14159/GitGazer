65068e074a680dfcea4192d58c4ec523fb994ee8
Michael Munsch <Mi3.1415926@gmail.com>
2024-04-07 10:42:45 +0200

    chore: delete old auth stack

--- 02_github_auth_api/src/dynamodb.ts
+++
@@ -1,6 +0,0 @@
-import {DynamoDBClient} from '@aws-sdk/client-dynamodb';
-import {DynamoDBDocumentClient} from '@aws-sdk/lib-dynamodb';
-
-const client = new DynamoDBClient({});
-const docClient = DynamoDBDocumentClient.from(client);
-export const getClient = (): DynamoDBDocumentClient => docClient;
--- 02_github_auth_api/src/logger.ts
+++
@@ -1,9 +0,0 @@
-export const getLogger = () => {
-    return console;
-}
-
-export const loggable = (memberName = 'logger') => {
-    return (target: Function): void => {
-        target.prototype[memberName] = 'Console';
-    };
-};
