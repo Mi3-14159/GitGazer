2e941f22d2bdf66701e48143d9644535ec7dad29
Michael Munsch <Mi3.1415926@gmail.com>
2024-04-14 12:56:30 +0200

    feat: refactor frontend

--- 04_frontend/src/App.vue
+++ 04_frontend/src/App.vue
@@ -1,129 +1,123 @@
 <script setup lang="ts">
-import { Auth, API } from "aws-amplify";
-import { ref } from "vue";
+import { ref } from 'vue'
+import { Amplify } from 'aws-amplify'
+import { Hub } from 'aws-amplify/utils'
+import {
+    signInWithRedirect,
+    signOut,
+    getCurrentUser,
+    type AuthUser,
+} from 'aws-amplify/auth'
 
-interface User {
-  attributes: {
-    name: string;
-    email: string;
-    picture: string;
-    nickname: string;
-  };
-}
+Amplify.configure({
+    Auth: {
+        Cognito: {
+            userPoolId: 'eu-central-1_kgbFsqjOi',
+            userPoolClientId: '1asi0t7vvdk8sochfdpunnon7b',
+            signUpVerificationMethod: 'code',
+            loginWith: {
+                oauth: {
+                    domain: `gitgazer-default.auth.eu-central-1.amazoncognito.com`,
+                    scopes: [
+                        'email',
+                        'profile',
+                        'openid',
+                        'aws.cognito.signin.user.admin',
+                    ],
+                    redirectSignIn: [import.meta.env.VITE_HOST_URL],
+                    redirectSignOut: [import.meta.env.VITE_HOST_URL],
+                    responseType: 'code',
+                },
+            },
+        },
+    },
+})
 
-const user = ref<User>({ attributes: { name: "", email: "", picture: "", nickname: "" } });
-const loading = ref(true);
+const user = ref<AuthUser>()
 
 const getUser = async () => {
-  const u = await Auth.currentUserInfo();
-  console.log(u);
-  if (u) {
-    user.value = u;
-  }
-  loading.value = false;
-};
-
-const handleSignOut = async () => await Auth.signOut();
-
-const handleSignIn = async () =>
-  await Auth.federatedSignIn({
-    customProvider: "Github",
-  });
-
-const handlePublicRequest = async () => {
-    const response = await API.get("api", "/public", {});
-    alert(JSON.stringify(response));
-};
-
-const handlePrivateRequest = async () => {
     try {
-      const response = await API.get("api", "/private", {
-        headers: {
-          Authorization: `Bearer ${(await Auth.currentSession())
-            .getAccessToken()
-            .getJwtToken()}`,
-        },
-      });
-      alert(JSON.stringify(response));
+        const currentUser = await getCurrentUser()
+        user.value = currentUser
     } catch (error) {
-      alert(error);
+        console.error(error)
+        console.log('Not signed in')
     }
-};
+}
 
-getUser();
-
+getUser()
 </script>
 
 <template>
-  <div v-if="loading">Loading...</div>
-  <div v-else className="container">
-    <h2>SST + Cognito + GitHub OAuth + React</h2>
-      <div v-if="user.attributes.name" className="profile">
-        <p>Welcome {{user.attributes.name}} aka {{ user.attributes.nickname }} !</p>
-        <p>{{user.attributes.email}}</p>
-        <button :onClick="handleSignOut">logout</button>
-      </div>
-      <div v-else>
-        <p>Not signed in</p>
-        <button :onClick="handleSignIn">login</button>
-      </div>
-    <div className="api-section">
-      <button :onClick="handlePublicRequest">call /public</button>
-      <button :onClick="handlePrivateRequest">call /private</button>
+    <div className="container">
+        <h2>Cognito + GitHub OAuth</h2>
+        <div v-if="user?.userId" className="profile">
+            <p>Welcome {{ user.userId }} aka {{ user.username }} !</p>
+            <button :onClick="() => signOut()">logout</button>
+        </div>
+        <div v-else>
+            <p>Not signed in</p>
+            <button
+                :onClick="
+                    () => signInWithRedirect({ provider: { custom: 'Github' } })
+                "
+            >
+                login
+            </button>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
 body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-    "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-    "Helvetica Neue", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+        'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+        'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 
 code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
-    monospace;
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+        monospace;
 }
 
 .container {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
 }
 
 button {
-  width: 120px;
-  padding: 10px;
-  border: none;
-  border-radius: 4px;
-  background-color: #000;
-  color: #fff;
-  font-size: 16px;
-  cursor: pointer;
+    width: 120px;
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+    background-color: #000;
+    color: #fff;
+    font-size: 16px;
+    cursor: pointer;
 }
 
 .profile {
-  border: 1px solid #ccc;
-  padding: 20px;
-  border-radius: 4px;
+    border: 1px solid #ccc;
+    padding: 20px;
+    border-radius: 4px;
 }
 .api-section {
-  width: 100%;
-  margin-top: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
+    width: 100%;
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
 }
 
 .api-section > button {
-  background-color: darkorange;
+    background-color: darkorange;
 }
 </style>
--- 04_frontend/src/main.ts
+++ 04_frontend/src/main.ts
@@ -3,40 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import { Amplify } from 'aws-amplify'
-
-Amplify.configure({
-    Auth: {
-        region: 'eu-central-1',
-        userPoolId: 'eu-central-1_kgbFsqjOi',
-        userPoolWebClientId: '1asi0t7vvdk8sochfdpunnon7b',
-        mandatorySignIn: false,
-        oauth: {
-            domain: `gitgazer-default.auth.eu-central-1.amazoncognito.com`,
-            scope: [
-                'email',
-                'profile',
-                'openid',
-                'aws.cognito.signin.user.admin',
-            ],
-            /*redirectSignIn: 'http://localhost:5173',
-            redirectSignOut: 'http://localhost:5173',*/
-            redirectSignIn: 'https://gitgazer.mmunsch.services',
-            redirectSignOut: 'https://gitgazer.mmunsch.services',
-            responseType: 'code',
-        },
-    },
-    API: {
-        endpoints: [
-            {
-                name: 'api',
-                endpoint:
-                    'https://c1fnd5q4vh.execute-api.eu-central-1.amazonaws.com/v1',
-                region: 'eu-central-1',
-            },
-        ],
-    },
-})
 
 const app = createApp(App)
 
