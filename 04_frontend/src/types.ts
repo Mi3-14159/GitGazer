e1a2f7cbadaecd3425bff3b0519d6f8cbf69862a
Michael Munsch <Mi3.1415926@gmail.com>
2024-05-07 23:50:31 +0200

    feat: introduce gql types

---
+++ 04_frontend/src/types.ts
@@ -0,0 +1,15 @@
+export interface Job {
+    run_id: number
+    action: string
+    workflow_name: string
+}
+
+export interface listJobsResponse {
+    listJobs: {
+        items: Job[]
+    }
+}
+
+export interface onPutJobSubscriptionResponse {
+    onPutJob: Job
+}
