7fd84197d141b7f065715e127e73ef6d585f782b
Michael Munsch <Mi3.1415926@gmail.com>
2025-09-21 09:31:37 +0200

    chore(eslint): 🔧 add TypeScript support and update linting scripts

--- 04_frontend/eslint.config.js
+++ 04_frontend/eslint.config.js
@@ -1,5 +1,19 @@
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+
 export default [
     {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: typescriptParser,
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': typescriptPlugin,
+        },
         rules: {
             semi: 'error',
             'prefer-const': 'error',
