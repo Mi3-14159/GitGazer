3e590ecb360ef45ef890c05e761f6126d5fe425e
Michael Munsch <Mi3.1415926@gmail.com>
2025-09-21 14:11:28 +0200

    fix(vite): 🔧 add `defer` option for Google Fonts in Vite config

--- 04_frontend/vite.config.ts
+++ 04_frontend/vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
                     {
                         name: 'Roboto',
                         styles: 'wght@100;300;400;500;700;900',
+                        defer: true,
                     },
                 ],
             },
