feea04f29fde00177bd68bef87410e37e0058354
Michael Munsch <Mi3.1415926@gmail.com>
2025-12-16 11:30:51 +0100

    feat(firehose): ✨ add Kinesis Firehose delivery stream with IAM roles and logging

--- infra/s3_analytics.tf
+++ infra/s3_analytics.tf
@@ -78,3 +78,160 @@ resource "aws_athena_data_catalog" "this" {
     catalog-id = "s3tablescatalog/${aws_s3tables_table_bucket.analytics.name}"
   }
 }
+
+resource "aws_s3_bucket" "firehose_logs" {
+  bucket        = "${data.aws_caller_identity.current.account_id}-${var.name_prefix}-firehose-logs-${terraform.workspace}"
+  force_destroy = true
+}
+
+resource "aws_cloudwatch_log_group" "firehose_analytics" {
+  name              = "/aws/kinesisfirehose/${var.name_prefix}-jobs-stream-${terraform.workspace}"
+  retention_in_days = 30
+}
+
+resource "aws_cloudwatch_log_stream" "firehose_analytics" {
+  name           = "DestinationDelivery"
+  log_group_name = aws_cloudwatch_log_group.firehose_analytics.name
+}
+
+resource "aws_iam_role" "firehose" {
+  name = "${var.name_prefix}-firehose-${terraform.workspace}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "firehose.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "firehose" {
+  name = "${var.name_prefix}-firehose-policy-${terraform.workspace}"
+  role = aws_iam_role.firehose.id
+
+  policy = data.aws_iam_policy_document.firehose_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "firehose_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3tables:GetTable",
+      "s3tables:PutTableData",
+      "s3tables:GetNamespace",
+      "s3tables:GetTableBucket"
+    ]
+    resources = [
+      aws_s3tables_table_bucket.analytics.arn,
+      "${aws_s3tables_table_bucket.analytics.arn}/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:PutObject"
+    ]
+    resources = [
+      aws_s3_bucket.firehose_logs.arn,
+      "${aws_s3_bucket.firehose_logs.arn}/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:GenerateDataKey",
+      "kms:Decrypt"
+    ]
+    resources = [
+      aws_kms_key.this.arn
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "glue:GetTable",
+      "glue:GetTableVersion",
+      "glue:GetTableVersions",
+      "glue:GetDatabase"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      aws_cloudwatch_log_group.firehose_analytics.arn,
+      "${aws_cloudwatch_log_group.firehose_analytics.arn}:*"
+    ]
+  }
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "jobs" {
+  name        = "${var.name_prefix}-jobs-stream-${terraform.workspace}"
+  destination = "iceberg"
+
+  iceberg_configuration {
+    append_only = false
+    # TODO: fetch ARN from a terraform resource
+    catalog_arn = "arn:aws:glue:${var.aws_region}:${data.aws_caller_identity.current.account_id}:catalog/s3tablescatalog/${aws_s3tables_table_bucket.analytics.name}"
+    role_arn    = aws_iam_role.firehose.arn
+
+    cloudwatch_logging_options {
+      enabled         = true
+      log_group_name  = aws_cloudwatch_log_group.firehose_analytics.name
+      log_stream_name = aws_cloudwatch_log_stream.firehose_analytics.name
+    }
+
+    processing_configuration {
+      enabled = true
+
+      processors {
+        type = "MetadataExtraction"
+
+        parameters {
+          parameter_name  = "JsonParsingEngine"
+          parameter_value = "JQ-1.6"
+        }
+        parameters {
+          parameter_name  = "MetadataExtractionQuery"
+          parameter_value = "{destinationDatabaseName:\"${aws_s3tables_namespace.gitgazer.namespace}\",destinationTableName:\"${aws_s3tables_table.jobs.name}\"}"
+        }
+      }
+    }
+
+    s3_configuration {
+      role_arn   = aws_iam_role.firehose.arn
+      bucket_arn = aws_s3_bucket.firehose_logs.arn
+      cloudwatch_logging_options {
+        enabled         = true
+        log_group_name  = aws_cloudwatch_log_group.firehose_analytics.name
+        log_stream_name = aws_cloudwatch_log_stream.firehose_analytics.name
+      }
+    }
+  }
+
+  server_side_encryption {
+    enabled  = true
+    key_type = "CUSTOMER_MANAGED_CMK"
+    key_arn  = aws_kms_key.this.arn
+  }
+}
