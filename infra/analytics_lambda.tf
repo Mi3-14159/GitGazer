95b49ad4d827f30cb92cc1698df456c3983fc0b0
Michael Munsch <Mi3.1415926@gmail.com>
2025-12-18 13:40:46 +0100

    feat(analytics): ✨ enable `aws_lambda_event_source_mapping` for job stream processing

--- infra/analytics_lambda.tf
+++ infra/analytics_lambda.tf
@@ -113,7 +113,7 @@ resource "aws_lambda_event_source_mapping" "analytics_jobs_stream" {
   starting_position              = "TRIM_HORIZON"
   batch_size                     = 100 # 500 ist the max limit of records a PutRecordBatchCommand can handle
   maximum_retry_attempts         = 3
-  enabled                        = false
+  enabled                        = true
   bisect_batch_on_function_error = true
   function_response_types = [
     "ReportBatchItemFailures",
