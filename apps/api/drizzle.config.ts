f972bfd8c8724bfe565761460cc04d8b860d95eb
Michael Munsch <Mi3.1415926@gmail.com>
2026-04-07 22:45:57 +0200

    refactor(config): 🔧 restructure database credentials handling and remove unused parameters

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,18 +1,28 @@
 import {config} from 'dotenv';
 import {defineConfig} from 'drizzle-kit';
+import {execSync} from 'node:child_process';
 
 config({path: process.env.ENV_FILE});
+
+const host = process.env['RDS_PROXY_ENDPOINT']!;
+const hostname = process.env['RDS_PROXY_HOSTNAME'] || host;
+const database = process.env['RDS_DATABASE']!;
+const user = process.env['RDS_DB_USER']!;
+
+const password = execSync(`aws rds generate-db-auth-token --hostname ${hostname} --port 5432 --username ${user}`, {encoding: 'utf-8'}).trim();
 
 export default defineConfig({
     out: './drizzle',
     schema: '../../packages/db/src/schema/index.ts',
     schemaFilter: ['gitgazer', 'github'],
     dialect: 'postgresql',
-    driver: 'aws-data-api',
-    breakpoints: true,
+    breakpoints: false,
     dbCredentials: {
-        database: process.env['RDS_DATABASE']!,
-        secretArn: process.env['RDS_SECRET_ARN']!,
-        resourceArn: process.env['RDS_RESOURCE_ARN']!,
+        host,
+        port: 5432,
+        database,
+        user,
+        password,
+        ssl: 'prefer',
     },
 });
